53f19eb52aaaa32132be6146a49d97517157bc38
Gordon Beeming <gordon@devstarops.com>
2022-09-18 21:26:14 +0200

    db create or attach if it exists already

--- .devcontainer/dev/init.ps1
+++ .devcontainer/dev/init.ps1
@@ -1,20 +1,39 @@
 #requires -PSEdition Core
-# Additional steps to initialize the development container
 
-# Install sqlserver module so we can use 'Invoke-Sqlcmd'
 Install-Module sqlserver -Confirm:$False  -Force
 
-# Invoke-Sqlcmd -ServerInstance "openveer_db,1433" -Username SA -Password P@ssw0rd12345 -Query "SELECT * FROM sys.databases";
-
-# Create new database
-# Source for the retry logic:
-# https://stackoverflow.com/a/47712807/411428
 $attempts = 20
 $sleepInSeconds = 3
 do {
   try {
-    Invoke-Sqlcmd -ServerInstance "openveer_db,1433" -Username SA -Password "P@ssw0rd12345" -Query "CREATE DATABASE [OpenVeer]";
-    Write-Host "Database created successfully."
+    if (Test-Path "/app/openveer_db/OpenVeer.mdf") {
+      Invoke-Sqlcmd -ServerInstance "openveer_db,1433" -Username SA -Password "P@ssw0rd12345" -Query @"
+    CREATE DATABASE [OpenVeer]   
+    ON (FILENAME = '/db/OpenVeer.mdf'),   
+        (FILENAME = '/db/OpenVeer.ldf')   
+    FOR ATTACH;  
+"@;
+      Write-Host "Database attached successfully."
+    }
+    else {
+      Invoke-Sqlcmd -ServerInstance "openveer_db,1433" -Username SA -Password "P@ssw0rd12345" -Query @"
+    CREATE DATABASE [OpenVeer]
+    ON
+    ( NAME = Solutions_dat,  
+        FILENAME = '/db/OpenVeer.mdf',
+        SIZE = 10MB,
+        MAXSIZE = 50MB,
+        FILEGROWTH = 5MB )  
+    LOG ON
+    ( NAME = Solutions_log,  
+        FILENAME = '/db/OpenVeer.ldf',
+        SIZE = 5MB,
+        MAXSIZE = 25MB,
+        FILEGROWTH = 5MB );
+    GO
+"@;
+      Write-Host "Database created successfully."
+    }
     break;
   }
   catch [Exception] {
@@ -27,6 +46,3 @@ do {
 
 . /app/.devcontainer/dev/build.ps1
 . /app/.devcontainer/dev/local-publish.ps1
-# Invoke-Sqlcmd -ServerInstance "openveer_db,1433" -Username SA -Password "P@ssw0rd12345" -InputFile "/app/.devcontainer/dev/init.sql";
-
-# dotnet new --install MSBuild.Sdk.SqlProj.Templates
